00f392f1d1d3bedecb6c4035e21e69e5d2be7b3c
JorgeGuzz <jguzmanv01@uc.cl>
2022-10-13 23:26:20 -0300

    initial commit

---
+++ carga_datos/Yt.py
@@ -0,0 +1,10 @@
+def obtener_Yt():
+    with open("datos/Yt.csv") as archivo:
+        cumple = archivo.readline()
+        cumple = cumple.strip().split(',')
+        cumple = list(map(lambda x: int(x), cumple))
+    return cumple
+
+if __name__ == "__main__":
+    cumple = obtener_Yt()
+    print({(t): cumple[t - 1] for t in range(1, 2000 + 1)})
---
+++ datos/parametros.py
@@ -0,0 +1,26 @@
+# PARAMETROS SIN SUBINDICE
+
+# ------------------------- CONJUNTOS --------------------------
+# numero de edificios
+n = 10
+# numero hasta donde llegan los edificios de decil bajo
+m = 4
+# numero de terrenos
+p = 2000
+# numero de comunas 
+l = 6
+# numero de cada servicio 
+s1 = 600
+s2 = 18
+s3 = 10
+s4 = 78
+
+# ------------------------- OTROS --------------------------
+PR = 22000000
+
+TMIN = 10
+
+DISTMAX = 
+
+MINCOM = 
+
